8a083e6b6ef0375881dffd6c139b1d1abd3702a8
send_wish <ubuntu@ip-172-31-9-164.ap-northeast-2.compute.internal>
2023-01-04 07:50:11 +0000

    feat: webscrap api

--- selenium_main.py
+++ selenium_main.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium_price import get_price
 from selenium_title import get_title
 from selenium_img import get_img
+from flask import jsonify
 
 DRIVER_PATH = "/chromedriver"
 options = Options()
@@ -17,15 +18,19 @@ mobile_emulation = { "deviceName": "iPhone X" }
 options.add_experimental_option("mobileEmulation", mobile_emulation)
 browser = webdriver.Chrome(options = options, executable_path=DRIVER_PATH)
 
-url = "https://www.kurly.com/goods/5065323" # 마켓컬리
+# url = "https://www.kurly.com/goods/5065323" # 마켓컬리
 # url = "https://m.smartstore.naver.com/yongsan/products/7762895541?NaPm=ct%3Dlcefumtc%7Cci%3D4098bec1f843d7e3e0eaeebb2dbccf4d57ad9585%7Ctr%3Dslbrc%7Csn%3D958071%7Chk%3Db15086f2940e945b34c9023fc3eea1adaf9ea118" # 네이버
 # url = "https://www.musinsa.com/app/goods/2826643" # 무신사
 # url = "https://m.11st.co.kr/products/m/2218563579" # 11번가 (price : x) => bysite
 # url = "https://m.coupang.com/vm/products/6335468410?vendorItemId=80524121180&sourceType=HOME_PERSONALIZED_ADS&searchId=feed-8e73a28dd2b1491a8291b0372d0e3ec4-personalized_ads&clickEventId=4b164f80-c05c-4f34-9181-10972e2c1914&isAddedCart="
-browser.get(url)
 
-title = get_title(browser, url)
-price = get_price(browser)
-img = get_img(browser, url)
+def web_scrap(url):
+    browser.get(url)
 
-browser.quit()
+    title = get_title(browser, url)
+    price = get_price(browser)
+    img = get_img(browser, url)
+
+    browser.quit()
+
+    return jsonify({'url': url, 'title': title, 'price': price, 'img': img})
