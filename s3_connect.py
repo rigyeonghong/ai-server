63c0bf8cd8456623873b4a6ab56ce2b8d09b9651
rigyeonghong <zxcvbee@naver.com>
2023-01-15 16:25:44 +0000

    fix: default image & remove comment

--- s3_connect.py
+++ s3_connect.py
@@ -29,4 +29,4 @@ def s3_img_upload(s3,f):
 def s3_get_img_url(filename):
     return f"https://{BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/{filename}"
 
-default_img = f"https://{BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/default_img.jpeg"
+default_img = f"https://{BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/default_image.png"
