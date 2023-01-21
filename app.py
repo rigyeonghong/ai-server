4c521edb3864198941f8d63fa438d029e5258d2b
Ubuntu <ubuntu@ip-172-31-36-234.ap-northeast-2.compute.internal>
2023-01-21 13:37:25 +0000

    fix:categorization ai

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from selenium_main import web_scrap
+from machine_learning import categorization
 
 app = Flask(__name__)
 
@@ -15,7 +16,11 @@ def webscrap():
     print("url", url_receive)
     # [todo] 예외처리 필요
     print("===Start Scraping===")
-    return web_scrap(url_receive)
+    result = web_scrap(url_receive)
+    print("===Start categorization===")
+    category = categorization(result['img'])
+    result['category'] = category
+    return result
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
--- selenium_main.py
+++ selenium_main.py
@@ -8,6 +8,7 @@ from flask import jsonify
 from elevenst import elevenst_get_info
 
 DRIVER_PATH = "/app/chrome/chromedriver"
+# DRIVER_PATH = "/chromedriver"
 options = Options()
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -20,9 +21,9 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 mobile_emulation = { "deviceName": "iPhone X" }
 options.add_experimental_option("mobileEmulation", mobile_emulation)
-browser = webdriver.Chrome(options = options, executable_path=DRIVER_PATH)
 
-def web_scrap(url): 
+def web_scrap(url):     
+    browser = webdriver.Chrome(options = options, executable_path=DRIVER_PATH)
     if (url.find("musinsaapp") != -1): # 무신사 앱링크면
         url += "?_imcp=1"
     browser.get(url)
